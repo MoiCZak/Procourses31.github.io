7a5071339f9c124473744a9a492f4ee9ecf36744
MoiCZak <zakariboy31@hotmail.fr>
2021-08-22 21:38:41 +0200

    restriction sanitaire fix

--- action_contact.php
+++ action_contact.php
@@ -1,24 +1,20 @@
 <?php
-if(!empty($_POST["send"])) {
+
     $name = $_POST["firstname"];
     $email = $_POST["email"];
     $objet = $_POST["objet"];
     $message = $_POST["message"];
-    $toEmail = "";
+    $toEmail = "zakariboy31@gmail.com";
     $mailHeaders = "From: " . $name . "<". $email .">\r\n";
     if(mail($toEmail, $objet, $message, $mailHeaders)) {
         echo "Votre message a bien été envoyé !";
-        header('index.html');
+        header("refresh:5;url=index.html");
     }else{
         echo "Erreur lors de l'envoi de l'e-mail.";
-        header('index.html');
+        header("refresh:5;url=index.html");
 
     }
-}else{
-    echo"Tous les champs doivent être complétés !";
-    header('index.html');
 
-}
 
 
 ?>
